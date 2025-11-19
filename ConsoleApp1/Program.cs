1d440c2db5a986228321ee5856d858d982d1972a
Devit-rai <Davidrai806@gmail.com>
2025-11-19 08:21:45 +0545

    Task 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student("David", 20, "Computing");
+        Student s2 = new Student("John", 22, "Networking");
+
+        Console.WriteLine("Student 1:");
+        Console.WriteLine(s1.name);
+        Console.WriteLine(s1.age);
+        Console.WriteLine(s1.course);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Student 2:");
+        Console.WriteLine(s2.name);
+        Console.WriteLine(s2.age);
+        Console.WriteLine(s2.course);
+
+        Console.WriteLine();
+
+        Console.WriteLine("College: " + Student.collegeName);
+    }
+}
